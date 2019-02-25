7adacb8b6f2886cd0a766c182ea511b1752b4943
root <root@Tadi-PC.localdomain>
2019-02-25 14:04:57 +0200

    Added Display Menu FFunction

---
+++ Student.cpp
@@ -0,0 +1,13 @@
+
+
+namespace MZRTAD001
+{
+    struct StudentRecord
+    {
+        char[40] Name;
+        char[40] Surname; 
+        char[9] StudentNumber;
+        char[20] ClassRecord;
+    }
+
+}
---
+++ StudentDBProgram.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// Function Defintions
+
+void DisplayMenu(void);
+
+namespace MZRTAD001
+{
+
+    int main void
+    {
+        int optionSelected;
+
+        DisplayMenu();
+
+        return 0;
+    }
+
+    void DisplayMenu()
+    {
+        cout<<"0: Add student";
+        cout<<"1: Read database";
+        cout<<"2: Save database";
+        cout<<"3: Display given student data";
+        cout<<"4: Grade student";
+        
+    }
+}
